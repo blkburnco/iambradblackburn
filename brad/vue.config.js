3e8a6d49e6ebc885be0936fa370c3f6268613c60
Kaska <fuzzybuttchimps@gmail.com>
2019-10-18 06:27:03 -0400

    routing for production app

--- brad/vue.config.js
+++ brad/vue.config.js
@@ -1,10 +1,29 @@
-// module.exports = {
-//   runtimeCompiler: true,
-//   outputDir: 'ghP'
-// }
-
 module.exports = {
+  runtimeCompiler: true,
+  outputDir: 'ghP',
   publicPath: process.env.NODE_ENV === 'production'
   ? '/iambradblackburn/brad/'
-  : '/'
+  : '/',
+
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/index/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    subpage: 'src/subpage/main.js'
+  }
 }
