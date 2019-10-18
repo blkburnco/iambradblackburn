00f7257fed331db553b4c2b7c87dc82ed011d636
Kaska <fuzzybuttchimps@gmail.com>
2019-10-18 06:48:14 -0400

    next attempt build prod

--- brad/vue.config.js
+++ brad/vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   pages: {
     index: {
       // entry for the page
-      entry: 'src/index/main.js',
+      entry: 'src/main.js',
       // the source template
       template: 'public/index.html',
       // output as dist/index.html
@@ -24,6 +24,6 @@ module.exports = {
     // template is inferred to be `public/subpage.html`
     // and falls back to `public/index.html` if not found.
     // Output filename is inferred to be `subpage.html`.
-    subpage: 'src/subpage/main.js'
+    //subpage: 'src/subpage/main.js'
   }
 }
